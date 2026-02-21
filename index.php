b44c561abb2c6ea2d03f3bc9590fd6850abb3361
InfernoidPl4y <infernoidpl4y@gmail.com>
2026-02-21 17:59:15 -0500

    update

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
   #Example how use MDFS
   include("MDFS/recv.php");
-  $MDFS= new MDFS_Scanner();
+  $MDFS= new MDFS();
   #Update Example
   if(date("w")==1){
     $MDFS->MDFS_UpdateLists("MDFS/BCL");
   }
   $ip_c=$_SERVER["REMOTE_ADDR"];
   $a=["1",2,"3","<script>"];
-  if($MDFS->MDFS_tmpScanner($MDFS->MDFS_listLoad("MDFS/BCL/MDFS_basic_list.txt"))=="CI"){
+  if($MDFS->MDFS_Scanner($MDFS->MDFS_listLoad("MDFS/BCL/MDFS_basic_list.txt"))=="CI"){
     echo "<span style='color: red'> [*]Block IP: $ip_c </span><br>";
   }else{
   echo "<span style='color: green'> [+]All OK</span><br>";
